8a9e9b2f4d4bc0f02d8840c8ba6f1087b790debf
Clinton Judy <clinton@j-udy.com>
2013-02-28 09:42:05 -0500

    Skip sending daily report if organization didn't have orders.

--- app/models/job/daily_email_report_job.rb
+++ app/models/job/daily_email_report_job.rb
@@ -2,6 +2,7 @@ class DailyEmailReportJob
   def self.perform
     Organization.all.each do |org|
       daily_report = DailyReport.new(org)
+      next if daily_report.rows.empty?
       ReportsMailer.daily(daily_report).deliver
     end
   end
