b2cae8e517a77dd2a7a2c533da40d7ab70f6d3aa
Gary Moore <gary.moore@gmail.com>
2013-04-15 15:43:09 -0400

    Second pass at nongift_migration

--- db/migrate/20130415190224_make_nongift_amount0_by_default.rb
+++ db/migrate/20130415190224_make_nongift_amount0_by_default.rb
@@ -1,7 +1,10 @@
 class MakeNongiftAmount0ByDefault < ActiveRecord::Migration
   def up
+    change_column_default :items, :nongift_amount, 0
+    ActiveRecord::Base.connection.execute "UPDATE items SET nongift_amount=0 WHERE nongift_amount IS NULL;"
   end
 
   def down
+    change_column :items, :nongift_amount, :integer, :default => nil
   end
 end
