6f17de752532fcccf2d4e13a521ac5d1298948bc
Gary Moore <gary.moore@gmail.com>
2013-04-15 10:50:14 -0400

    Adding core_ext to support def_each method

---
+++ lib/artfully_ose/core_ext.rb
@@ -0,0 +1,10 @@
+class Class
+  # Easily add multiple methods that do the same thing.
+  def def_each(*method_names, &block)
+    method_names.each do |method_name|
+      define_method method_name do
+        instance_exec method_name, &block
+      end
+    end
+  end
+end
